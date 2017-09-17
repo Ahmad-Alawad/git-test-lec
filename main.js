210ae2cd17b315e55b52f3761c67e18f42babf25
Ahmad Alawad <ahmad.alawad.sf@gmail.com>
2017-09-17 15:44:47 +0300

    added .ready to make sure all elements are loaded before

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-var pic = $('#img');
-var picBtn = $('#change-pic-btn');
+// to make sure elements are loaded before
+$document.ready( function(){ 
+	var pic = $('#img');
+	var picBtn = $('#change-pic-btn');
 
-picBtn.click(function(){pic.src = 'https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg';
+	// Event Handler to change the src of image
+	picBtn.click(function(){
+		pic.src = 'https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg';
+	});
 });
